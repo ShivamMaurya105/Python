ad793ac462c888678d9c01687b893a99d159402e
Shivam Maurya <mauryashivam105@gmail.com>
2021-12-20 21:03:35 +0530

    Python code to music player

---
+++ practice.py
@@ -0,0 +1,24 @@
+'''a="Shivam"
+for i in range(len(a)):
+    print(a[i])'''
+
+'''a="Shivam"
+for i in range(len(a)):
+    print(a[-i-1])
+    len(a)'''
+
+'''num=int(input("Enter a number:"))
+if(num%2)==0:
+    print("Number is even")
+else:python3
+    print("Number is odd")'''
+
+num=str(input("Enter the name:"))
+if(len(num)%2)==0:
+    print("The name is even")
+else:
+    print("The name is odd")
+
+
+
+
